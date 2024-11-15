e484c242927291d60233432b18e72ef9428e2d9d
nayoung16 <nykim1016@sookmyung.ac.kr>
2024-11-15 15:24:34 +0900

    BE init

---
+++ src/main/java/com/example/startlight/StartlightApplication.java
@@ -0,0 +1,13 @@
+package com.example.startlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartlightApplication.class, args);
+	}
+
+}

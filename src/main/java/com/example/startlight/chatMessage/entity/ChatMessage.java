25591b482be13318f9409b2aac3af9d5efc2d2ba
nayoung16 <nykim1016@sookmyung.ac.kr>
2024-12-24 13:43:28 +0900

    ChatMessage, ChatSession

---
+++ src/main/java/com/example/startlight/chatMessage/entity/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.example.startlight.chatMessage.entity;
+
+import com.example.startlight.chatSession.entity.ChatSession;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chatMessage")
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long chatmsg_id;
+
+    @ManyToOne
+    private ChatSession chatSession;
+
+    @Column(nullable = false)
+    private Boolean memberSent;
+
+    @Column(nullable = false)
+    private String message;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}

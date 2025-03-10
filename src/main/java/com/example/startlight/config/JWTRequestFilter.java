859f80bc299c0580e6126f03ae92c66042bbe042
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-03-11 00:56:30 +0900

    resolve conflict @Autowired

--- src/main/java/com/example/startlight/config/JWTRequestFilter.java
+++ src/main/java/com/example/startlight/config/JWTRequestFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;

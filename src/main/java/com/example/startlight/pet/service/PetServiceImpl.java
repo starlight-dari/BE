723f5d3b92452a832d5c30df8707c8631ff5ad31
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-03-15 01:23:47 +0900

    feat : ml 서버 재배포

--- src/main/java/com/example/startlight/pet/service/PetServiceImpl.java
+++ src/main/java/com/example/startlight/pet/service/PetServiceImpl.java
@@ -40,7 +40,6 @@ public class PetServiceImpl implements PetService{
         String uploadFile = s3Service.uploadPetImg(petReqDto.getPet_img(), String.valueOf(pet.getPet_id()));
         pet.setPet_img(uploadFile);
 
-        /*
         // Step 1: 첫 번째 Flask API 호출
         String flaskApiUrl = flaskService.apiUrl+ "/stars_run_pidinet";
         Map<String, String> requestBody = new HashMap<>();
@@ -72,7 +71,7 @@ public class PetServiceImpl implements PetService{
             System.out.println("❌ Flask 서버에서 응답 실패: " + response.getStatusCode());
             throw new RuntimeException("Flask 서버 응답 실패");
         }
-         */
+        /*
         FlaskResponseDto flaskResponseDto = flaskService.testMLApi();
         pet.setSvg_path(flaskResponseDto.getSvgPath());
         List<Edge> edges = flaskResponseDto.getEdges().stream()
@@ -82,6 +81,7 @@ public class PetServiceImpl implements PetService{
         List<StarListRepDto> list = starListService.createList(pet.getPet_id(), flaskResponseDto.getMajorPoints());
         return PetIdRepDto.builder()
                 .petId(pet.getPet_id()).build();
+                */
     }
 
     @Override

a39bdc8c823a899a5d21fcc0ca4d2e5d3b129e3b
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-03-19 01:06:54 +0900

    #40 feat : memory album

--- src/main/java/com/example/startlight/memoryAlbum/controller/MemoryAlbumController.java
+++ src/main/java/com/example/startlight/memoryAlbum/controller/MemoryAlbumController.java
@@ -1,19 +1,33 @@
 package com.example.startlight.memoryAlbum.controller;
 
+import com.example.startlight.memoryAlbum.dto.AlbumByPetRepDto;
 import com.example.startlight.memoryAlbum.service.MemoryAlbumFlaskService;
+import com.example.startlight.memoryAlbum.service.MemoryAlbumService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("memory-album")
 public class MemoryAlbumController {
     private final MemoryAlbumFlaskService memoryAlbumFlaskService;
+    private final MemoryAlbumService memoryAlbumService;
 
-    @PostMapping()
-    public void createMemoryAlbum() {
+    @GetMapping()
+    public ResponseEntity<?> createMemoryAlbum() {
         memoryAlbumFlaskService.generateMemoryAlbum();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<List<AlbumByPetRepDto>> getAllMemoryAlbumStatus() {
+        List<AlbumByPetRepDto> memoryAlbumStatusByPet = memoryAlbumService.getMemoryAlbumStatusByPet();
+        return ResponseEntity.ok(memoryAlbumStatusByPet);
     }
 }
--- src/main/java/com/example/startlight/memoryAlbum/repository/MemoryAlbumRepository.java
+++ src/main/java/com/example/startlight/memoryAlbum/repository/MemoryAlbumRepository.java
@@ -1,10 +1,17 @@
 package com.example.startlight.memoryAlbum.repository;
 
 import com.example.startlight.memoryAlbum.entity.MemoryAlbum;
+import com.example.startlight.pet.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemoryAlbumRepository extends JpaRepository<MemoryAlbum, Long> {
-    public List<MemoryAlbum>
+    @Query("select ma from MemoryAlbum ma where ma.pet.pet_id = :petId")
+    List<MemoryAlbum> findAllByPetId(@Param("petId") Long petId);
+
+    @Query("select count(ma) from MemoryAlbum ma where ma.pet.pet_id = :petId and ma.opened = false")
+    Integer countNotOpenedByPetId(@Param("petId") Long petId);
 }

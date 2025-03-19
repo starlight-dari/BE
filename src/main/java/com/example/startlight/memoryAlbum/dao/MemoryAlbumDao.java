194ea5b61a681fc19d37e210a0f14f3b46547b4f
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-03-19 15:42:29 +0900

    #40 feat : Select memory album
    select by letter id, pet id
    create memory album

--- src/main/java/com/example/startlight/memoryAlbum/dao/MemoryAlbumDao.java
+++ src/main/java/com/example/startlight/memoryAlbum/dao/MemoryAlbumDao.java
@@ -2,10 +2,12 @@ package com.example.startlight.memoryAlbum.dao;
 
 import com.example.startlight.memoryAlbum.entity.MemoryAlbum;
 import com.example.startlight.memoryAlbum.repository.MemoryAlbumRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,5 +20,10 @@ public class MemoryAlbumDao {
 
     public List<MemoryAlbum> findByPetId(Long petId) {
         return memoryAlbumRepository.findAllByPetId(petId);
+    }
+
+    public MemoryAlbum findById(Long id) {
+        return memoryAlbumRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Album not found with id: " + id));
     }
 }
--- src/main/java/com/example/startlight/memoryAlbum/service/MemoryAlbumService.java
+++ src/main/java/com/example/startlight/memoryAlbum/service/MemoryAlbumService.java
@@ -2,21 +2,26 @@ package com.example.startlight.memoryAlbum.service;
 
 import com.example.startlight.memoryAlbum.dao.MemoryAlbumDao;
 import com.example.startlight.memoryAlbum.dto.AlbumByPetRepDto;
+import com.example.startlight.memoryAlbum.dto.MemoryAlbumRepDto;
+import com.example.startlight.memoryAlbum.dto.MemoryAlbumReqDto;
+import com.example.startlight.memoryAlbum.dto.MemoryAlbumSimpleDto;
 import com.example.startlight.memoryAlbum.entity.MemoryAlbum;
 import com.example.startlight.memoryAlbum.repository.MemoryAlbumRepository;
 import com.example.startlight.pet.dao.PetDao;
 import com.example.startlight.pet.entity.Pet;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemoryAlbumService {
     private final MemoryAlbumFlaskService memoryAlbumFlaskService;
     private final MemoryAlbumRepository memoryAlbumRepository;
+    private final MemoryAlbumDao memoryAlbumDao;
     private final PetDao petDao;
 
     public List<AlbumByPetRepDto> getMemoryAlbumStatusByPet() {
@@ -53,7 +58,47 @@ public class MemoryAlbumService {
        return albumByPetRepDtos;
     }
 
-    public void createAlbum() {
-        memoryAlbumFlaskService.generateMemoryAlbum();
+    public List<MemoryAlbumSimpleDto> getMemoryAlbumByPet(Long petId) {
+        List<MemoryAlbum> byPetId = memoryAlbumDao.findByPetId(petId);
+        return byPetId.stream()
+                .map(this::toSimpleDto)
+                .collect(Collectors.toList());
+    }
+
+    public MemoryAlbumRepDto getMemoryAlbum(Long letterId) {
+        MemoryAlbum byId = memoryAlbumDao.findById(letterId);
+        return toResponseDto(byId);
+
+    }
+
+    private MemoryAlbumSimpleDto toSimpleDto(MemoryAlbum memoryAlbum) {
+        return MemoryAlbumSimpleDto.builder()
+                .letter_id(memoryAlbum.getLetter_id())
+                .pet_id(memoryAlbum.getPet().getPet_id()) // Pet 엔티티에서 ID 매핑
+                .content(memoryAlbum.getContent())
+                .createdAt(memoryAlbum.getCreatedAt())
+                .opened(memoryAlbum.getOpened())
+                .build();
+    }
+
+    public MemoryAlbumRepDto createMemoryAlbum(MemoryAlbumReqDto memoryAlbumReqDto) {
+        Pet selectedPet = petDao.selectPet(memoryAlbumReqDto.getPet_id());
+        MemoryAlbum memoryAlbum = MemoryAlbum.builder()
+                .pet(selectedPet)
+                .content(memoryAlbumReqDto.getContent())
+                .images(memoryAlbumReqDto.getImages())
+                .build();
+        MemoryAlbum createdAlbum = memoryAlbumDao.createMemoryAlbum(memoryAlbum);
+        return toResponseDto(createdAlbum);
+    }
+
+    private MemoryAlbumRepDto toResponseDto(MemoryAlbum memoryAlbum) {
+        return MemoryAlbumRepDto.builder()
+                .letter_id(memoryAlbum.getLetter_id())
+                .pet_id(memoryAlbum.getPet().getPet_id())
+                .content(memoryAlbum.getContent())
+                .images(memoryAlbum.getImages())
+                .createdAt(memoryAlbum.getCreatedAt())
+                .opened(memoryAlbum.getOpened()).build();
     }
 }
